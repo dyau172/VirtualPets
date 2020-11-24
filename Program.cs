5aadb00fe02ecf1ed823ee1ca6ff87d93e8b13ea
dyau172 <47676379+dyau172@users.noreply.github.com>
2020-11-24 10:21:25 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace virtualpets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fish cakes!");
+        }
+    }
+}
