f8648cb9cb48b07afcca48aa65344d08e0486afa
dyau172 <47676379+dyau172@users.noreply.github.com>
2020-12-11 05:32:32 +0000

    Removed comments

--- Inventory.cs
+++ Inventory.cs
@@ -5,121 +5,86 @@ namespace virtualpets {
     public class Inventory {
         public List<Toys> toysPurchased = new List<Toys> ();
 
-        List<Medicine> MedicineList = new List<Medicine> ();
+        public List<Medicine> medicineList = new List<Medicine> ();
 
-        public int Coins;
-        //Room room = new Room ();
+        public int Coins { get; set; }
+        Room room;
+        Medicine meds;
+        Toys toy;
 
-        public Inventory () {
-
+        public Inventory (Room room) {
+            this.room = room;
+            
         }
-
         public string DisplayToys () {
-            //Print list of toys to purchase 
-
-            /*
-            int index = 0;
+      
             string toysp = "";
+            int index = 1;
             foreach (Toys toy in toysPurchased) {
+
+                toysp += $"{index} {toy.Name}\n   ";
                 index++;
-                toysp += $"{toy.Name}\n";
-            };
-            return index.ToString() + toysp;
-            */
-
-           /*  string toysp = "";
-            for (int i = 0; i < toysPurchased.Count; i++) {
-
-                foreach (Toys toy in toysPurchased) {
-
-                    toysp += $"{i} , {toy.Name}\n";
-                };
-            }
-            return toysp; */
-
-            string toysp = "";
-            foreach (Toys toy in toysPurchased) {
-
-                toysp += $"{toy.Name}\n   ";
             };
             return toysp;
-
         }
-
         public string DisplayMeds () {
             string medlist = "";
-            Console.SetCursorPosition (2, 16);
-            foreach (Medicine meds in MedicineList) {
-                medlist += $"{meds.Name}\n  ";
+            int index = 1;
+            
+            foreach (Medicine meds in medicineList) {
+                medlist += $"{index} {meds.Name}\n   ";
+                index++;
             };
             return medlist;
-
         }
-        public void PurchaseToys (Toys item) {
-            //add toy from list from purchase list
-            Console.WriteLine ("You want to purchase toys. Key 1 for ball/Key 2 for Slippers");
+        public void PlayToy (Inventory bag) {
+            //Select toy from list 
+            Console.SetCursorPosition (40, 16);
 
-            int key = Convert.ToInt32 (Console.ReadLine ());
+            Console.WriteLine ("Do you want to play with a toy? y/n");
+            char input = Console.ReadKey ().KeyChar;
 
-            if (key == 1) {
-                item = Dependancy.CreateBall ();
-                UpdateCoin (item);
-                toysPurchased.Add (item);
-                Console.WriteLine ("Ball added to bag");
-                //Coins = Coins - item.Cost;
+            if (input == 'y') {
+                Console.SetCursorPosition (40, 17);
+                Console.WriteLine ("Which toy do you want to use?");
+                Console.SetCursorPosition (40, 18);
+                int index = Convert.ToInt32 (Console.ReadLine ());
+                index -= 1;
+                this.toy = bag.toysPurchased[index];
 
-            } else if (key == 2) {
-                item = Dependancy.CreateSlipper ();
-                UpdateCoin (item);
-                toysPurchased.Add (item);
-                Console.WriteLine ("Slipper added to bag");
+                bag.toysPurchased.RemoveAt (index);
+  
+                room.pet.Happiness = room.pet.Happiness + toy.HappyValue;
 
-            } else
-                Console.WriteLine ("Ya did nuffin");
-
-        }
-
-        public void PurchaseMedcine (Medicine item) {
-            //add toy from list from purchase list
-            Console.WriteLine ("You want to purchase medicine. Key 3 for Cheapo Meds/Key 4 for the Expensive stuff");
-            int key = Convert.ToInt32 (Console.ReadLine ());
-
-            if (key == 3) {
-                item = Dependancy.CreateCheapMeds ();
-                UpdateCoinMeds (item);
-                MedicineList.Add (item);
-                Console.WriteLine ("Cheap Medicine added to bag");
-                //Coins = Coins - item.Cost;
-
-            } else if (key == 4) {
-                item = Dependancy.CreateExpMedicine ();
-                UpdateCoinMeds (item);
-                MedicineList.Add (item);
-                Console.WriteLine ("Expensive Medicine added to bag");
-
-            } else
-                Console.WriteLine ("Ya did nuffin");
-
-        }
-        public void UpdateCoin (Toys item) {
-            if (Coins > item.Cost) {
-                Coins = Coins - item.Cost;
             } else {
-                Console.WriteLine ("You don't have enough coins");
-                Console.ReadKey (true);
-
+                Console.WriteLine ("");
             }
 
         }
-        public void UpdateCoinMeds (Medicine item) {
-            if (Coins > item.Cost) {
-                Coins = Coins - item.Cost;
+        public void UseMeds (Inventory bag) {
+            //Select toy from list 
+            Console.SetCursorPosition (40, 16);
+
+            Console.WriteLine ("Do you want to cure world fish? y/n");
+            char input = Console.ReadKey ().KeyChar;
+
+            if (input == 'y') {
+                Console.SetCursorPosition (40, 18);
+                Console.WriteLine ("Which medicine do you want to use?");
+                int index = Convert.ToInt32 (Console.ReadLine ());
+                index -= 1;
+                 this.meds = bag.medicineList[index];
+
+                medicineList.RemoveAt (index);
+                Console.WriteLine ("room.pet.");
+
+                room.pet.Happiness = room.pet.Happiness + meds.ChangeHappiness;
+                room.pet.Hunger = room.pet.Hunger + meds.ChangeHunger;
+                room.pet.Healthy = true;
+
             } else {
-                Console.WriteLine ("You don't have enough coins");
-                Console.ReadKey (true);
-
+                Console.WriteLine ("");
             }
-
         }
 
     }
--- Toys.cs
+++ Toys.cs
@@ -7,44 +7,13 @@ namespace virtualpets {
         public int Cost { get; set; }
         public int HappyValue {get; set;}
 
-        Room room;
-        Inventory bag;
-
         public Toys (string name, int cost, int happyValue) {
             Name = name;
             Cost = cost;
             HappyValue = happyValue;
 
-            
         }
 
-       /*  public void PlayToy (Toys item) {
-            //Select toy from list 
-            Console.SetCursorPosition(0, 20);
-                 
-            Console.WriteLine ("Do you want to play with a toy? y/n");
-            char input = Console.ReadKey ().KeyChar;
-            
-            if (input == 'y') {
-                Console.WriteLine ("Which toy do you want to use?");
-                int index = Convert.ToInt32 (Console.ReadLine ());
-                index -= 1;
-
-                bag.toysPurchased.RemoveAt (index);
-                Console.WriteLine("room.pet.");
-                   
-                room.pet.Happiness = room.pet.Happiness + item.HappyValue;
-
-            }else if(input =='n'){
-                 Console.WriteLine("play.playtoy no");
-            }
-             else {
-                Console.WriteLine("play.playtoy else");
-            }
-          
-        } */
-
-      
                 
     }
 }
