6644ba231d074e0bd523a991d6f152868afddd2d
dyau172 <47676379+dyau172@users.noreply.github.com>
2020-12-11 05:33:16 +0000

    Moved method

--- Medicine.cs
+++ Medicine.cs
@@ -2,8 +2,7 @@ using System;
 
 namespace virtualpets {
     public class Medicine {
-        Inventory bag;
-        Room room;
+
         public string Name { get; set; }
         public int Cost { get; set; }
         public double ChangeHappiness { get; set; }
@@ -17,32 +16,6 @@ namespace virtualpets {
 
         }
 
-        public void UseMeds (Medicine meds) {
-            //Select toy from list 
-            Console.SetCursorPosition (0, 20);
-
-            Console.WriteLine ("Do you want to cure world fish? y/n");
-            char input = Console.ReadKey ().KeyChar;
-
-            if (input == 'y') {
-                Console.WriteLine ("Which medicine do you want to use?");
-                int index = Convert.ToInt32 (Console.ReadLine ());
-                index -= 1;
-
-                bag.toysPurchased.RemoveAt (index);
-                Console.WriteLine ("room.pet.");
-
-                room.pet.Happiness = room.pet.Happiness + meds.ChangeHappiness;
-                room.pet.Hunger = room.pet.Hunger + meds.ChangeHunger;
-                room.pet.Healthy = true;
-
-            } else if (input == 'n') {
-                Console.WriteLine ("medicine.useMeds no");
-            } else {
-                Console.WriteLine ("medicine.useMeds else");
-            }
-
-        }
-
+        
     }
 }
--- Shop.cs
+++ Shop.cs
@@ -4,42 +4,94 @@ using System.Collections.Generic;
 namespace virtualpets {
     public class Shop {
         //List of toys avaiable
-        public List<Toys> toysForSale = new List<Toys> ();
-        List<Medicine> medsForSale = new List<Medicine>();
-        Inventory bag = Dependancy.CreateInventory ();
+        List<Toys> toysForSale = new List<Toys> ();
+        List<Medicine> medsForSale = new List<Medicine> ();
+        Inventory bag;
         Toys ball = Dependancy.CreateBall ();
         Toys slipper = Dependancy.CreateSlipper ();
-        Medicine cheapMeds = Dependancy.CreateCheapMeds();
-        Medicine expMeds = Dependancy.CreateExpMedicine();
+        Medicine cheapMeds = Dependancy.CreateCheapMeds ();
+        Medicine expMeds = Dependancy.CreateExpMedicine ();
 
-       
-
-        
-
-        public Shop () {
-
-            toysForSale.Add(ball);
-            toysForSale.Add(slipper);
-            medsForSale.Add(cheapMeds);
-            medsForSale.Add(expMeds);
+        public Shop (Inventory bag) {
+            this.bag = bag;
+            toysForSale.Add (ball);
+            toysForSale.Add (slipper);
+            medsForSale.Add (cheapMeds);
+            medsForSale.Add (expMeds);
         }
         public string DisplayToys () {
             //Print list of toys to purchase
             string toylist = "";
-            Console.SetCursorPosition(2,14);
+            Console.SetCursorPosition (2, 14);
             foreach (Toys toy in toysForSale) {
                 toylist += $"{toy.Name}\n  ";
             };
             return toylist;
         }
-        public string DisplayMeds(){
-           string medlist = "";
-            Console.SetCursorPosition(2,16);
+        public string DisplayMeds () {
+            string medlist = "";
+            Console.SetCursorPosition (2, 16);
             foreach (Medicine meds in medsForSale) {
                 medlist += $"{meds.Name}\n  ";
             };
-            return medlist; 
+            return medlist;
         }
-    
+
+      
+        public void BuyStuff (Toys toy, Medicine meds) {
+            Console.WriteLine ("Buy things");
+
+            int key = Convert.ToInt32 (Console.ReadLine ());
+
+            if (key == 1) {
+                toy = Dependancy.CreateBall ();
+                UpdateCoin (toy);
+                bag.toysPurchased.Add (toy);
+
+            } else if (key == 2) {
+                toy = Dependancy.CreateSlipper ();
+                UpdateCoin (toy);
+                bag.toysPurchased.Add (toy);
+
+            } else if (key == 3) {
+                meds = Dependancy.CreateCheapMeds ();
+                UpdateCoinMeds (meds);
+                bag.medicineList.Add (meds);
+
+            } else if (key == 4) {
+                meds = Dependancy.CreateExpMedicine ();
+                UpdateCoinMeds (meds);
+                bag.medicineList.Add (meds);
+
+            } else
+                Console.WriteLine ("Ya did nuffin");
+                
+        }
+
+        public void UpdateCoin (Toys toy) {
+             if (bag.Coins >= toy.Cost) {
+                bag.Coins = bag.Coins - toy.Cost;
+                Console.WriteLine ("Toy added to bag");
+                Console.ReadKey(true);
+            } else {
+                Console.WriteLine ("You don't have enough coins for this toy");
+                Console.ReadKey (true);
+
+            }
+ 
+        }
+        public void UpdateCoinMeds (Medicine meds) {
+            if (bag.Coins >= meds.Cost) {
+                bag.Coins = bag.Coins - meds.Cost;
+                Console.WriteLine ("Medicine added to bag");
+                Console.ReadKey(true);
+            } else {
+                Console.WriteLine ("You don't have enough coins for the meds");
+                Console.ReadKey (true);
+            }
+
+        }
+
+
     }
 }
